3a5a9810f2eaca26b494a5d9fc69f47c357e4d04
Paula <43395223+PaulaE89@users.noreply.github.com>
2020-03-11 22:17:14 -0500

    add show-list-gifs

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 
 document.getElementById("arrow_dropdown").onclick=show_hide;
-document.getElementById("input_search_gifs").onkeypress=show_search;
+document.getElementById("input_search_gifs").onkeyup=show_search;
 document.getElementById("sailor_night").onclick=change_style;
 document.getElementById("sailor_day").onclick=change_white;
+
+
+var first_search_bar= document.getElementById("fname");
+
 
 var click= document.getElementById("dropdown-content");
 var show_result_gifs=document.getElementById("show_result_gifs");
@@ -22,7 +26,54 @@ function show_hide(){
    
 }
 
-function show_search(){
+
+
+function show_search(e){
+
+    if(e.target.value.length>2){
+
+        show_result_gifs.classList.add("show-list-gifs");
+
+
+        console.log("add class show");
+
+        onFocusout();
+
+
+
+ 
+
+
+    }else{
+
+        show_result_gifs.classList.remove("show-list-gifs");
+
+    }
+    
+
+}
+
+
+
+
+
+ function onFocusout(e){
+
+ if(e.target){
+
+ 
+    
+    show_result_gifs.classList.remove("show-list-gifs");
+ }
+
+
+}
+
+
+
+
+/*function show_search(){
+
 
     if(show_result_gifs.style.display==="none"){
 
@@ -32,7 +83,7 @@ function show_search(){
         show_result_gifs.style.display="none";
     }
 
-}
+}*/
 
 
 
@@ -45,7 +96,7 @@ estilos.href='./css/style_nigth.css ';
 function change_white(){
 
 
-    estilos.href='./css/style.css'
+    estilos.href='./css/style.css';
 
 }
 
