07138277818705d0eb892804dbb5f04e9dbbda71
Paula <43395223+PaulaE89@users.noreply.github.com>
2020-03-15 23:09:03 -0500

    add trending

--- js/script.js
+++ js/script.js
@@ -9,53 +9,78 @@ var show_result_gifs=document.getElementById("show_result_gifs");
 var estilos=document.getElementById('estilos');
 
 
-var first_image=document.querySelector("#first_image");
+var add_images=document.getElementsByClassName("add_image");
 
-var gifs=1;
+var gifs=4;
+
+
+var tendencias=10;
 
 /*calling APIS'S GIFOS */
 
-
-fetch('http://api.giphy.com/v1/stickers/trending?api_key=pDbe98unNhbtcnfymALov2ljqZoF2Ztf&limit=' + gifs)
+function trending(){
+const found= fetch('http://api.giphy.com/v1/stickers/trending?api_key=pDbe98unNhbtcnfymALov2ljqZoF2Ztf&limit=' + gifs)
 .then((response)=>{
-
-    
-    return  response.json();;
+      return  response.json();
 }).then((response)=>{
 
     console.log(response);
-    
-
-    console.log("the first position" + ""  + "id" + " " + response.data[0].id);
-
-    console.log(response.data.length);
-    
     var total_gifs=response.data.length;
  
-
-
     for (var i=0; i< total_gifs;i++){
-
-    console.log("this is total gifs" + "  " +total_gifs);
-
-   var prueba= response.data[0].images.fixed_height_small_still.url;
-
-   first_image.src=prueba;
-    console.log(first_image);
-
-
+               
+        add_images[i].src= response.data[i].images.fixed_height_small_still.url;
     }
-
-    
-    
-
-
-
 
 }).catch(error =>{
 
     return error;
 });
+}
+
+function tendencias(){
+
+    const pruebafound= fetch('http://api.giphy.com/v1/stickers/trending?api_key=pDbe98unNhbtcnfymALov2ljqZoF2Ztf&limit=' + tendencias)
+    .then ((response)=>{
+
+        return response.json();
+
+    }).then((response)=>{
+
+ var total_gifs=response.data.length;
+
+ for (var i=0; i<total_gifs;i++){
+
+
+    const prueba=
+
+
+
+    `<div class="card" data-id=${i}>
+    <img src="${response.data[i].images.fixed_height_small_still.url}" class="img-thumbnail thumb m-r" width="100" height="100">
+    
+    </div>`
+
+
+    const html = document.implementation.createHTMLDocument();
+    html.body.innerHTML = prueba;
+    image.append(html.body.children[0]);
+
+ }
+
+
+
+
+
+    }).catch(error=>{
+
+    });
+
+
+}
+
+
+
 
 
 
@@ -76,19 +101,15 @@ function show_hide(){
 function show_search(e){
 
     if(e.target.value.length>2){
-
         show_result_gifs.classList.add("show-list-gifs");
-
         input_search_gifs.addEventListener('focusout',(event)=>{
-
             show_result_gifs.classList.remove("show-list-gifs");
-                    
+         
             });
             
     }else{
 
         show_result_gifs.classList.remove("show-list-gifs");
-
     }
  }
 
@@ -105,3 +126,4 @@ function change_white(){
 
 
 
+trending();
