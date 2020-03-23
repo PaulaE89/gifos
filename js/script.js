9bdcdbe9f20c9a5a0f269aa6cf94ea38f11ffde5
Paula <43395223+PaulaE89@users.noreply.github.com>
2020-03-23 15:24:02 -0500

    add  new search

--- js/script.js
+++ js/script.js
@@ -11,8 +11,15 @@ var tendencias_list = document.getElementById("tendencias");
 var add_images = document.getElementsByClassName("add_image");
 
 var button_search=document.getElementById("search-btn");
+
+
+
+var section=document.getElementsByClassName("advice");
 var gifs = 12;
-var tendencias_count = 12;
+
+console.log(section);
+
+var prueba;
 
 /*calling APIS'S GIFOS */
 
@@ -25,13 +32,13 @@ function trending() {
             console.log(response);
             for (var i = 0; i < 4; i++) {
 
-                add_images[i].src = response.data[i].images.downsized_large.url;
+                add_images[i].src = response.data[i].images.original.url;
             }
             /*prueba */
             for (var i = 4; i < gifs; i++) {
                 const cards =
                     `<div class="card" data-id=${i}>
-        <img src="${response.data[i].images.downsized_large.url}" class="img-thumbnail thumb m-r" >
+        <img src="${response.data[i].images.original.url}" class="img-thumbnail thumb m-r" >
          </div>`
                 const html = document.implementation.createHTMLDocument();
                 html.body.innerHTML = cards;
@@ -61,38 +68,85 @@ function show_hide() {
 }
 
 
-function prueba() {
+function search() {
 
     input_search_gifs.addEventListener('keyup', (e) => {
 
         var new_search = e.target.value;
+        prueba=new_search;
+        console.log(prueba);
 
-        console.log(new_search);
+       if(button_search.addEventListener("click",filter_search)|| button_search.addEventListener("keydown", function(e){
+           if(event.key==="enter"){
 
+            console.log("poraqui");
+               filter_search(new_search);
+           
+       }
+    }));
 
-        button_search.addEventListener("click", (e) => {
+      /* button_search.addEventListener("click", (e) => {
 
         fetch('http://api.giphy.com/v1/gifs/search?api_key=pDbe98unNhbtcnfymALov2ljqZoF2Ztf&limit=2&q=' + new_search)
         .then((respuesta) => {
 
-            console.log(respuesta);
+           
             return respuesta.json();
 
         }).then((respuesta) => {
 
 
-            console.log("exito"+ " " + respuesta);
+            console.log( respuesta);
 
            
         });
 
-    });
+              });*/
 });
 
     
 
 
 }
+
+
+function filter_search(e){
+
+    console.log("this is pruebas " + prueba);
+
+
+    fetch('http://api.giphy.com/v1/gifs/search?api_key=pDbe98unNhbtcnfymALov2ljqZoF2Ztf&limit=2&q=' + prueba)
+        .then((respuesta) => {
+
+            
+            return respuesta.json();
+
+        }).then((respuesta) => {
+
+            delete_information();
+            console.log( respuesta);
+
+           
+        });
+
+    }
+
+
+
+
+    function delete_information(){
+        
+        section.style.display="none";
+
+
+
+
+    }
+
+
+
+
+
 
 
 function show_search(e) {
@@ -121,4 +175,4 @@ function change_white() {
 
 
 trending();
-prueba();
+search();
